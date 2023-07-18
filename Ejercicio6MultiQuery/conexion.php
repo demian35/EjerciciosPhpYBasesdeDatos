216a6eb1ac6446f01722edd125e51ca23ce6ca40
Demian <53890358+demian35@users.noreply.github.com>
2023-07-18 03:35:24 -0600

    conexion a la bd

---
+++ Ejercicio6MultiQuery/conexion.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Ejercicio 1 conexion a bd mysql mediante mysqli
+*/
+
+$server="localhost";//nombre del server
+$usuario="root";//Usuario
+$password="root";//contraseña
+$baseDeDatos="basededatospractica";//nombre de la base de datos en donde trabajaremos
+
+$conexion= new mysqli($server,$usuario,$password,$baseDeDatos); //creamos nuestra conexion tipo mysqli
+
+
+
+//probamos que la conexion sea exitosa
+if($conexion->connect_error){
+    die("Opps ha ocurrido un error al tratar de conectar".$conexion->connect_error);//si falla mandamos error
+}else{
+    echo "Conexion exitosa";//si no confimamos que la conexion ha sido exitosa
+}
+
+
+?>
