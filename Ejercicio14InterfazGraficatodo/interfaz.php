177c86f6a41edb8a1d699285bb129bf737901560
Demian <53890358+demian35@users.noreply.github.com>
2023-07-19 03:33:01 -0600

    Creando la interfaz del todo con bootstrap

---
+++ Ejercicio14InterfazGraficatodo/interfaz.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administra tus tareas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+        <div class="card">
+            <div class="card-header">
+            
+            </div>
+            <div class="card-body">
+                <form action=""  method="post">
+                    ingrese una tarea: <input required class="form-control" type="text" name="nombre" id="">
+                    <br>
+                    <button class="btn btn-success" type="submit">Envia tarea</button>
+                    <br>
+                </form>
+            </div>
+            <div class="card-footer text-muted">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="col-md-6">
+    <div class="table-responsive">
+        <div class="table-responsive">
+            <table class="table table-primary">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Tarea</th>
+                        <th>Accion</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="">
+                        <td>1</td>
+                        <td>Una tarea</td>
+                        <td><a name="" id="" class="btn btn-danger" href="" role="button">Borrar</a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        
+
+    </div>
+</div>
+
+</body>
+</html>
